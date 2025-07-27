a55c3c12247aa7836e52bb3186d5c3a25eb3aa9e
Molo312004 <312004molo@gmail.com>
2025-07-28 01:06:53 +0530

    Initial commit: PixelForge Nexus project

---
+++ src/main/java/com/pixelforge/PixelforgeApp.java
@@ -0,0 +1,12 @@
+
+package com.pixelforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelforgeApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PixelforgeApp.class, args);
+    }
+}
